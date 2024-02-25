c48fa46b401e2ec6868d5f9dfe665a0bd47018b6
blue_fen <1310772574@qq.com>
2024-02-26 02:04:16 +0800

    initial commit

---
+++ demo/src/main/java/blue/fen/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package blue.fen.demo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
